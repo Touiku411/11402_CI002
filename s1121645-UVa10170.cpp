3e8764eb0c8e0e5f768cd3e3062e0b55c74cb568
Touiku411 <tengyu081229@gmail.com>
2026-03-10 11:51:08 +0800

    Add files via upload

---
+++ s1121645-UVa10170.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+	int cnt = 1;
+	while (n--)
+	{
+		int a, b;
+		cin >> a >> b;
+		int sum = 0;
+		for (int i = a; i <= b; ++i) {
+			if (i % 2 != 0) {
+				sum += i;
+			}
+		}
+		cout << "Case " << cnt++ << ": ";
+		cout << sum << endl;
+	}
+}
