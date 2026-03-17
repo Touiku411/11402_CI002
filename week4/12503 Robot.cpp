50272a639e33506baaa9fa1bf3da34a3f679f217
Touiku411 <tengyu081229@gmail.com>
2026-03-17 11:47:08 +0800

    Add files via upload

---
+++ week4/12503 Robot.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int n;
+		cin >> n;
+		cin.ignore();
+		string instruct;
+		vector<string> vec;
+		int pos = 0;
+
+		while (n--) {
+			cin >> instruct;
+			if (instruct == "LEFT")	{
+				pos--;
+				vec.push_back(instruct);
+			}
+			else if (instruct == "RIGHT") {
+				pos++;
+				vec.push_back(instruct);
+			}
+			else {
+				string s1;
+				int num;
+				cin >> s1 >> num;
+				if (num > 0) {
+					int idx = num - 1;
+					vec.push_back(vec[idx]);
+					if (vec[idx] == "RIGHT") {
+						pos++;
+					}
+					else {
+						pos--;
+					}
+				}
+			}
+		}
+		cout << pos << endl;
+	}
+}
+
