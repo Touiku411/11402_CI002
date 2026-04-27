dd644dc2b6df0e6ae6ad6eb23c8ffdba91a7c3bf
Touiku411 <tengyu081229@gmail.com>
2026-04-28 02:02:10 +0800

    Create s1121645-LeetCode394.cpp

---
+++ Before-Class_Exercises/Week10_13_LeetCode/src/s1121645-LeetCode394.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> numStk;
+        stack<string> strStk;
+        int curNum = 0;
+        string curStr = "";
+        for(char c: s){
+            if(isdigit(c)){
+                curNum = curNum * 10 + (c - '0');
+            }
+            else if( isalpha(c)){
+                curStr+=c;
+            }
+            else if(c == '['){
+                numStk.push(curNum);
+                strStk.push(curStr);
+                curNum = 0;
+                curStr = "";
+            }
+            else if(c == ']'){
+                string prev = strStk.top();
+                strStk.pop();
+                int repeatTimes = numStk.top();
+                numStk.pop();
+                string temp = "";
+                for(int i = 0 ; i < repeatTimes; ++i){
+                    temp += curStr;
+                }
+                curStr = prev + temp;
+            }
+        }
+        return curStr;
+    }
+};
